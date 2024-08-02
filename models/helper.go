9a795ed61ea3a94a554a09a4de26f12500dd7ee3
nanda03dev <nanda03dev@gmail.com>
2024-08-02 10:55:17 +0530

    fix: minor

--- models/helper.go
+++ models/helper.go
@@ -101,18 +101,18 @@ func GetCheckProcess(entityType common.EntityNameType, operationType common.Oper
 }
 
 func GetStringValue(document gnosql_client.Document, key string) string {
-	value, _ := document[key]
+	value := document[key]
 	return value.(string)
 }
 func GetIntegerValue(document gnosql_client.Document, key string) int {
-	value, _ := document[key]
+	value := document[key]
 	return value.(int)
 }
 func GetBoolValue(document gnosql_client.Document, key string) bool {
-	value, _ := document[key]
+	value := document[key]
 	return value.(bool)
 }
 func GetValue[T any](document gnosql_client.Document, key string) T {
-	value, _ := document[key]
+	value := document[key]
 	return value.(T)
 }
